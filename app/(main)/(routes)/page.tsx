fbb385baa50cd4a61c3f0501d91941ae8c95d1da
VincentHuynh1016 <VincentVinhHuynh@gmail.com>
2025-07-17 16:15:06 -0700

    add dropdown toggle

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,7 +1,10 @@
+import { UserButton } from "@clerk/nextjs";
+import { ModeToggle } from "@/components/mode-toggle";
 export default function Home() {
   return (
     <div>
-      This is a protected route
+      <UserButton />
+      <ModeToggle />
     </div>
   );
 }
